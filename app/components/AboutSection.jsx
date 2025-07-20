3f9c374f832bf568be9cc279f2856e98efec0770
carefulcoder123 <pawarsankalp2003@gmail.com>
2025-07-20 21:42:35 +0530

    Aligned Who is Sankalp in about section to center in mobile screens

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -9,11 +9,10 @@ export default function JourneySection() {
       id="about"
       className="min-h-screen flex items-start py-20 px-6 md:px-20 scroll-mt-24"
     >
-      
-  <div className="max-w-6xl mx-auto">
+<div className="max-w-6xl mx-auto text-center md:text-left"> {/* Added px-4 and text-center */}
   <SplitText
     text="Who is Sankalp?"
-    className="text-4xl md:text-5xl font-bold mb-16 text-center h-[60px] text-indigo-500"
+    className="text-3xl sm:text-4xl md:text-5xl  font-bold mb-16 text-indigo-500"
     delay={100}
     duration={0.6}
     ease="power3.out"
@@ -22,10 +21,9 @@ export default function JourneySection() {
     to={{ opacity: 1, y: 0 }}
     threshold={0.1}
     rootMargin="-100px"
-    textAlign="center"
   />
 
-  <div className="grid md:grid-cols-2 gap-10 items-center">
+  <div className="grid md:grid-cols-2 gap-10 items-center text-left"> {/* text-left for paragraphs */}
     {/* Text Section */}
     <motion.div
       initial={{ opacity: 0, y: 30 }}
@@ -34,13 +32,13 @@ export default function JourneySection() {
       viewport={{ once: true }}
     >
       <p className="text-gray-300 text-lg leading-relaxed text-justify">
-      I’m Sankalp, a web developer and automation enthusiast who believes in building smart, scalable solutions.  
-          I specialize in building modern, responsive web apps using React, Next.js, and Tailwind CSS, while ensuring performance and clean UI/UX.
-        <br /> <br />
-          With a strong foundation in Linux (RHCSA certified) and hands-on experience with automation using Ansible, I enjoy bridging the gap between development and infrastructure. I'm deeply curious about DevOps, open-source tools, and exploring how AI can enhance productivity
+        I’m Sankalp, a web developer and automation enthusiast who believes in building smart, scalable solutions.
+        I specialize in building modern, responsive web apps using React, Next.js, and Tailwind CSS, while ensuring performance and clean UI/UX.
+        <br /><br />
+        With a strong foundation in Linux (RHCSA certified) and hands-on experience with automation using Ansible, I enjoy bridging the gap between development and infrastructure. I'm deeply curious about DevOps, open-source tools, and exploring how AI can enhance productivity.
       </p>
       <p className="text-gray-400 mt-4">
-      Apart from development, I focus on enhancing workflows through automation and learning how to build with AI.
+        Apart from development, I focus on enhancing workflows through automation and learning how to build with AI.
       </p>
     </motion.div>
 
